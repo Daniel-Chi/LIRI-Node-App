93257f50dc6ade93403a80b1f7fa2849d7b88e05
Daniel Chi <danielhchi@gmail.com>
2019-04-15 16:06:36 -0400

    finished spotify

--- liri.js
+++ liri.js
@@ -1,3 +1,4 @@
+//node and keys module setup
 require("dotenv").config();
 let keys = require("./keys");
 let axios = require("axios");
@@ -8,12 +9,16 @@ let options = {
     apiKey: "IzlTGWTE7IniSptaCFTuUE8NKK6sLk0m"
 };
 let geocoder = NodeGeocoder(options);
+let Spotify = require("node-spotify-api");
+let spotify = new Spotify(keys.spotify);
 
-require("node-spotify-api");
-// let spotify = new Spotify(keys.spotify);
-
+//command line arguments, a is command type, b is search term
 let a = process.argv[2];
-let b = process.argv[3];
+let arrb = [];
+for (i = 3; i < process.argv.length; i++) {
+    arrb.push(process.argv[i]);
+};
+let b = arrb.join(" ");
 
 //bands in town api command
 if (a === "concert-this") {
@@ -22,10 +27,11 @@ if (a === "concert-this") {
             for (i = 0; i < res.data.length; i++) {
                 let name = res.data[i].venue.name;
                 let date = moment(res.data[i].datetime).format("MM/DD/YYYY");
-                geocoder.reverse({lat: res.data[i].venue.latitude, lon: res.data[i].venue.longitude}, 
-                    function (err, resp){
-                        console.log("\n"+resp[0].streetName+", "+resp[0].city+" "+resp[0].stateCode+", "+resp[0].countryCode);
-                    }).then(function(){
+                geocoder.reverse({ lat: res.data[i].venue.latitude, lon: res.data[i].venue.longitude },
+                    function (err, data) {
+                        if (err) { return console.log("Error occurred: " + err); };
+                        console.log("\n" + data[0].streetName + ", " + data[0].city + " " + data[0].stateCode + ", " + data[0].countryCode);
+                    }).then(function () {
                         console.log(name);
                         console.log(date);
                     });
@@ -34,11 +40,26 @@ if (a === "concert-this") {
 }
 //spotify api command
 else if (a === "spotify-this-song") {
-
+    //set default
+    if (b.length === 0) {
+        b = "The Sign Ace of Base";
+    };
+    spotify.search({ type: "track", query: b }, function (err, data) {
+        if (err) { return console.log("Error occurred: " + err); };
+        let track = data.tracks.items[0]
+        console.log("\nTrack: " + track.name)
+        console.log("Album: " + track.album.name);
+        console.log("Artist: " + track.artists[0].name);
+        console.log("Link: " + track.external_urls.spotify);
+    });
 }
 //omdb api command
 else if (a === "movie-this") {
-
+    //set default
+    if (b.length === 0) {
+        b = "Mr. Nobody";
+    };
+    axios.get()
 }
 //reads random.txt for command
 else if (a === "do-what-it-says") {
