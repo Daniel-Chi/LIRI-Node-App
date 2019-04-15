f5295f6204aac12bba23ae0c45a6b77a115bf556
Daniel Chi <danielhchi@gmail.com>
2019-04-15 15:10:44 -0400

    finished bands in town

--- liri.js
+++ liri.js
@@ -1,14 +1,36 @@
 require("dotenv").config();
 let keys = require("./keys");
 let axios = require("axios");
-let spotify = new Spotify(keys.spotify);
+let moment = require("moment");
+let NodeGeocoder = require("node-geocoder");
+let options = {
+    provider: "mapquest",
+    apiKey: "IzlTGWTE7IniSptaCFTuUE8NKK6sLk0m"
+};
+let geocoder = NodeGeocoder(options);
+
+require("node-spotify-api");
+// let spotify = new Spotify(keys.spotify);
 
 let a = process.argv[2];
 let b = process.argv[3];
 
 //bands in town api command
 if (a === "concert-this") {
-
+    axios.get("https://rest.bandsintown.com/artists/" + b + "/events?app_id=codingbootcamp")
+        .then(function (res) {
+            for (i = 0; i < res.data.length; i++) {
+                let name = res.data[i].venue.name;
+                let date = moment(res.data[i].datetime).format("MM/DD/YYYY");
+                geocoder.reverse({lat: res.data[i].venue.latitude, lon: res.data[i].venue.longitude}, 
+                    function (err, resp){
+                        console.log("\n"+resp[0].streetName+", "+resp[0].city+" "+resp[0].stateCode+", "+resp[0].countryCode);
+                    }).then(function(){
+                        console.log(name);
+                        console.log(date);
+                    });
+            };
+        });
 }
 //spotify api command
 else if (a === "spotify-this-song") {
